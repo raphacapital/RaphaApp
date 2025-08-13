5fd583d4ac7802ff8632bb96eba8ed287a6e6787
cmturne4 <carsonturnersf@gmail.com>
2025-08-13 11:24:02 -0500

    Fix font paths in fontLoader.ts after moving fonts to assets directory

--- Desktop/Rapha/utils/fontLoader.ts
+++ Desktop/Rapha/utils/fontLoader.ts
@@ -2,10 +2,10 @@ import * as Font from 'expo-font';
 
 export const loadFonts = async () => {
   await Font.loadAsync({
-    'NeueHaasDisplayRoman': require('../assets/fonts/NeueHaasDisplayRoman.ttf'),
-    'NeueHaasDisplayMediu': require('../assets/fonts/NeueHaasDisplayMediu.ttf'),
-    'NeueHaasDisplayBold': require('../assets/fonts/NeueHaasDisplayBold.ttf'),
-    'NeueHaasDisplayLight': require('../assets/fonts/NeueHaasDisplayLight.ttf'),
-    'NeueHaasDisplayThin': require('../assets/fonts/NeueHaasDisplayThin.ttf'),
+    'NeueHaasDisplayRoman': require('./assets/fonts/NeueHaasDisplayRoman.ttf'),
+    'NeueHaasDisplayMediu': require('./assets/fonts/NeueHaasDisplayMediu.ttf'),
+    'NeueHaasDisplayBold': require('./assets/fonts/NeueHaasDisplayBold.ttf'),
+    'NeueHaasDisplayLight': require('./assets/fonts/NeueHaasDisplayLight.ttf'),
+    'NeueHaasDisplayThin': require('./assets/fonts/NeueHaasDisplayThin.ttf'),
   });
 };
